0caf26edf511216b7ef6bcf2500f79e79e94befd
maoling <maoling@apache.org>
2025-01-05 18:27:00 +0800

    02-db-pretend to tear down and install etcd v2

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -1,34 +1,34 @@
 (ns jepsen.etcdemo
-  (:require [clojure.tools.logging :refer :all]
-  					[clojure,string :as str]
-  					[jepsen [cli :as cli]
-						        [control :as c]
-						        [db :as db]
-						        [tests :as tests]]
-            [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
+		(:require [clojure.tools.logging :refer :all]
+			[clojure.string :as str]
+			[jepsen [cli :as cli]
+			 [control :as c]
+			 [db :as db]
+			 [tests :as tests]]
+			[jepsen.control.util :as cu]
+			[jepsen.os.debian :as debian]))
 
 (defn db
-		"Etcd DB for a particular version."
-		[version]
-		(reify db/DB
-				(setup! [_ test node]
-					(info node "installing etcd" version))
-				
-				 (teardown! [_ test node]
-				 	(info node "tearing down etcd"))))
-		
+			"Etcd DB for a particular version."
+			[version]
+			(reify db/DB
+						 (setup! [_ test node]
+										 (info node "installing etcd" version))
+
+						 (teardown! [_ test node]
+												(info node "tearing down etcd"))))
+
 
 (defn etcd-test
-  "Given an options map from the command line runner (e.g. :nodes, :ssh,
-  :concurrency, ...), constructs a test map."
-  [opts]
-  (merge tests/noop-test
-				 opts
-         {:name "etcd"
-          :os debian/os
-          :db (db "v3.1.5")
-         	:pure-generators true}))
+			"Given an options map from the command line runner (e.g. :nodes, :ssh,
+      :concurrency ...), constructs a test map."
+			[opts]
+			(merge tests/noop-test
+						 opts
+						 {:name "etcd"
+							:os   debian/os
+							:db   (db "v3.1.5")
+							:pure-generators true}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
