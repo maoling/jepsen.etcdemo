f78c8c9eed310c12bae400371166f8f118e3490c
maoling <maoling@apache.org>
2025-01-06 18:58:07 +0800

    02-db: initial-cluster

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -9,6 +9,33 @@
 			[jepsen.os.debian :as debian]))
 
 (def dir "/opt/etcd")
+(def binary "etcd")
+(def logfile (str dir "/etcd.log"))
+(def pidfile (str dir "/etcd.pid"))
+
+(defn node-url
+			"An HTTP url for connecting to a node on a particular port."
+			[node port]
+			(str "http://" node ":" port))
+
+(defn peer-url
+			"The HTTP url for other peers to talk to a node."
+			[node]
+			(node-url node 2380))
+
+(defn client-url
+			"The HTTP url clients use to talk to a node"
+			[node]
+			(node-url node 2379))
+
+(defn initial-cluster
+			"Constructs an initial cluster string for a test, like
+			\foo=foo:2380,bar=bar:2380,...\""
+			[test]
+			(->> (:nodes test)
+					 (map (fn [node]
+										(str node "=" (peer-url node))))
+					 (str/join ",")))
 
 (defn db
 			"Etcd DB for a particular version."
