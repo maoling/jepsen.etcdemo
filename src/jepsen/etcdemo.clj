9b8d512a01e3034afe2bf7ed83edb6d06a0c930c
maoling <maoling@apache.org>
2025-01-13 19:09:19 +0800

    03-client: Reads: add generator & r-w-cas function

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -24,6 +24,10 @@
 
 		(close! [_ test]))
 
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
 (def dir "/opt/etcd")
 (def binary "etcd")
 (def logfile (str dir "/etcd.log"))
