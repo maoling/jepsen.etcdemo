5cfd79316bb64b20a14d3402a553900ccbcf5abf
maoling <maoling@apache.org>
2025-01-19 21:51:08 +0800

    08-set. Integrating the New Workload

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -108,7 +108,10 @@
 
 (def cli-opts
 	"Additional command line options."
-	[["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
+	[["-w" "--workload NAME" "What workload should we run?"
+    :missing  (str "--workload " (cli/one-of workloads))
+    :validate [workloads (cli/one-of workloads)]]
+   ["-q" "--quorum" "Use quorum reads, instead of reading from any primary."]
 	 ["-r" "--rate HZ" "Approximate number of requests per second, per thread."
 		:default  10
 		:parse-fn read-string
@@ -118,33 +121,52 @@
 		:parse-fn parse-long
 		:validate [pos? "Must be a positive integer."]]])
 
+(defn register-workload
+      "Tests linearizable reads, writes, and compare-and-set operations on
+      independent keys."
+      [opts]
+      {:client    (Client. nil)
+       :checker   (independent/checker
+                    (checker/compose
+                      {:linear   (checker/linearizable {:model     (model/cas-register)
+                                                        :algorithm :linear})
+                       :timeline (timeline/html)}))
+       :generator (independent/concurrent-generator
+                    10
+                    (range)
+                    (fn [k]
+                        (->> (gen/mix [r w cas])
+                             (gen/limit (:ops-per-key opts)))))})
+
+(def workloads
+  "A map of workload names to functions that construct workloads, given opts."
+  {"set"      set/workload
+   "register" register-workload})
+
 (defn etcd-test
       "Given an options map from the command line runner (e.g. :nodes, :ssh,
       :concurrency ...), constructs a test map. Special options:
 
           :quorum       Whether to use quorum reads
           :rate         Approximate number of requests per second, per thread
-          :ops-per-key  Maximum number of operations allowed on any given key."
+          :ops-per-key  Maximum number of operations allowed on any given key
+          :workload     Type of workload."
       [opts]
       (let [quorum    (boolean (:quorum opts))
-            workload  (set/workload opts)]
+            workload  ((get workloads (:workload opts)) opts)]
            (merge tests/noop-test
                   opts
                   {:pure-generators true
-                   :name            (str "etcd q=" quorum)
-                   :quorum          quorum
-                   :os              debian/os
-                   :db              (db "v3.1.5")
-                   :client          (Client. nil)
-                   :nemesis         (nemesis/partition-random-halves)
-                   :checker         (checker/compose
-                                      {:perf   (checker/perf)
-                                       :indep (independent/checker
-                                                (checker/compose
-                                                  {:linear   (checker/linearizable
-                                                               {:model (model/cas-register)
-                                                                :algorithm :linear})
-                                                   :timeline (timeline/html)}))})
+                   :name       (str "etcd q=" quorum " "
+                                    (name (:workload opts)))
+                   :quorum     quorum
+                   :os         debian/os
+                   :db         (db "v3.1.5")
+                   :nemesis    (nemesis/partition-random-halves)
+                   :client     (:client workload)
+                   :checker    (checker/compose
+                                 {:perf     (checker/perf)
+                                  :workload (:checker workload)})
                    :generator       (->> (independent/concurrent-generator
                                            10
                                            (range)
